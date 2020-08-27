d1504841c1bc40be4d36978adceaee4aa08faec7
IQTrading <iqfirst.kiev@gmail.com>
2020-08-27 13:49:50 +0300

    27.08 end09

--- movies/urls.py
+++ movies/urls.py
@@ -6,4 +6,5 @@ from .views import *
 urlpatterns = [
     path("", MoviesView.as_view()),
     path("<slug:slug>/", MovieDetailView.as_view(), name='movie_detail'),
+    path("review/<int:pk>/", AddReview.as_view(), name="add_review"),
 ]
